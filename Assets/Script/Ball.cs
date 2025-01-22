4279297a004fe6648fed04d0b9b219015429eb06
alfonsosun <93112105+dunkiin@users.noreply.github.com>
2025-01-22 15:05:19 -0800

    Updated

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -3,18 +3,33 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody rb;
+    public float speed = 1.0f;
 
     int minX;
     int maxX;
 
+    int scoreP1;
+    int scoreP2;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Reset()
     {
  
     }
 
-    void Launch()
+    void Launch(Vector3 direction, float speed)
     {
         
     }
+
+    void GetRandomBallDirection()
+    {
+        
+    }
+
+
 }
