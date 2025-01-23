ee500f14652c582803b98504f34645901376206d
alfie <alfiesunn666@gmail.com>
2025-01-23 09:23:49 -0800

    Created UIManager

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI score;
+    
+    public void UpdateScore(int scoreP1, int scoreP2)
+    {
+        score.text = scoreP1 + " : " + scoreP2;
+    }
+}
