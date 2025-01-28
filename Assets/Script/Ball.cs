1601a6c8890f1ea9d421d35097ef4729ec53e3d8
alfie <alfiesunn666@gmail.com>
2025-01-28 11:50:06 -0800

    Update Ball.cs

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -29,18 +29,21 @@ public class Ball : MonoBehaviour
 
     Vector3 GetRandomBallDirection()
     {
-        float x = 1;
-        float y = 1;
+        // float x = 1;
+        // float y = 1;
 
-        if (Random.Range(0, 2) == 0)
-        {
-            x = -1f;
-        }
+        // if (Random.Range(0, 2) == 0)
+        // {
+        //     x = -1f;
+        // }
 
-        if (Random.Range(0, 2) == 0)
-        {
-            y = -1f;
-        }
+        // if (Random.Range(0, 2) == 0)
+        // {
+        //     y = -1f;
+        // }
+
+        float x = Random.Range(0, 2) == 0 ? -1f : 1f;
+        float y = Random.Range(0, 2) == 0 ? -1f : 1f;   
 
         Vector3 dir = new Vector3(x, y, 0);
         return dir;
